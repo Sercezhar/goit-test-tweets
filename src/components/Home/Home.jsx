cd038ae62a99aa69ba75ed9ae74d7473523a5a5b
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-04-17 15:14:19 +0300

    update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,16 +6,26 @@ function Home() {
   return (
     <div className={styles.home}>
       <div className={styles.wrapper}>
-        <h1 className={styles.title}>
-          <span className={styles.goit}>GOIT</span>{' '}
-          <span className={styles.tweets}>Tweets</span>
-        </h1>
+        <motion.div
+          initial={{ rotate: -20, scale: 0 }}
+          animate={{ rotate: 0, scale: 1 }}
+          transition={{
+            type: 'spring',
+            stiffness: 260,
+            damping: 20,
+          }}
+        >
+          <h1 className={styles.title}>
+            <span className={styles.goit}>GOIT</span>{' '}
+            <span className={styles.tweets}>Tweets</span>
+          </h1>
+        </motion.div>
 
         <motion.button
           className={styles.button}
           type="button"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
           exit={{ opacity: 0 }}
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 1 }}
